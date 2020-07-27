9f83aed73fd1bdc2a8e0196ff8073c2cdd3d7329
oqo <oqolarte@tuta.io>
2020-07-27 20:38:45 +0800

    clean up

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=13:antialias=true:autohint=true";
-static char *font2[] = { "SpaceMono:pixelsize=13:antialias=true:autohint=true" };
+static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font2[] = { "SpaceMono:pixelsize=14:antialias=true:autohint=true" };
 
 static int borderpx = 2;
 
