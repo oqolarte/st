8831fbaab4863a818c1aef0a35cca825848e928b
oqo <oqolarte@tuta.io>
2020-08-10 17:01:16 +0800

    increase font size of terminal

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "SpaceMono:pixelsize=14:antialias=true:autohint=true" };
+static char *font = "Liberation Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font2[] = { "SpaceMono:pixelsize=15:antialias=true:autohint=true" };
 
 static int borderpx = 2;
 
@@ -62,7 +62,7 @@ static double maxlatency = 33;
  * blinking timeout (set to 0 to disable blinking) for the terminal blinking
  * attribute.
  */
-static unsigned int blinktimeout = 800;
+static unsigned int blinktimeout = 500;
 
 /*
  * thickness of underline and bar cursors
